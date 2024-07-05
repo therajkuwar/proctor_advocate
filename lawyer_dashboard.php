a1ab151a1732ad7f788e0cfb8f774e314cab8fc2
Raj Kuwar <113436638+therajkuwar@users.noreply.github.com>
2024-07-05 15:23:10 +0530

    Add files via upload

---
+++ lawyer_dashboard.php
@@ -0,0 +1,120 @@
+<?php
+	session_start();
+	if($_SESSION['login']==TRUE AND $_SESSION['status']=='Active'){
+		
+		//session_start();
+		include("db_con/dbCon.php");
+		
+	?>
+	<!doctype html>
+	<html lang="en">
+		<head>
+			<!-- Required meta tags -->
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			
+			<!-- Bootstrap CSS -->
+			<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"> -->
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+			<link rel="stylesheet" href="css/all.css">
+			<link rel="stylesheet" href="css/simple-sidebar.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/style.css">
+			<link rel="stylesheet" href="css/media.css">
+			<title></title>
+		</head>
+		<body>
+			<header class="customnav bg-success">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<nav class="navbar navbar-expand-lg ">
+								<a class="navbar-brand cus-a" href="index.php">Proctor Advocate</a>
+								
+								
+								<div class="collapse navbar-collapse" id="navbarSupportedContent">
+									<ul class="navbar-nav ml-auto ">
+										<li class="">
+											<a class="nav-link cus-a" href="#">Full Name: <?php echo $_SESSION['first_Name'];?> <?php echo $_SESSION['last_Name'];?></a>
+										</li>
+										<li class="">
+											<a class="nav-link cus-a" href="logout.php">Log Out</a>
+										</li>
+										
+									</ul>
+									
+								</div>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</header>
+			<body>
+				
+				<div class="d-flex" id="wrapper">
+					
+					<!-- Sidebar -->
+					<div class="bg-light border-right" id="sidebar-wrapper">
+						<div class="sidebar-heading">My Profile</div>
+						<div class="list-group list-group-flush">
+							<a href="lawyer_dashboard.php" class="list-group-item list-group-item-action bg-light">Dashboard</a><!--lawyer dashboard page-->
+							<a href="lawyer_edit_profile.php" class="list-group-item list-group-item-action bg-light">Edit Profile</a><!--lawyer_edit_profile page-->
+							<a href="lawyer_booking.php" class="list-group-item list-group-item-action bg-light">Booking requests</a><!--this page-->
+							<a href="update_password_admin.php" class="list-group-item list-group-item-action bg-light">Update Password</a>
+						</div>
+					</div>
+					<!-- /#sidebar-wrapper -->
+					
+					<!-- Page Content -->
+					<div id="page-content-wrapper">
+						<?php if(isset($_GET['done'])){
+							echo "<div class='alert alert-success alert-dismissible fade show'>
+							<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+							<strong>Welcome!</strong> You are login as Lawyer.
+							</div>";
+						}?>
+						<div class="container-fluid">
+						<h1 class="mt-4"><strong>Welcome to Your Profile</strong></h1>
+						<p>Welcome! Here on your dashboard, you have access to all the tools and features you need to manage your legal practice efficiently and effectively. Whether you're representing clients, managing cases, or connecting with potential clients, we're here to support you every step of the way.</p>
+						<h2 class="mt-3">Manage Your Legal Practice</h2>
+						<p>Manage all aspects of your legal practice conveniently from your dashboard. Keep track of your ongoing cases, communicate with your clients, schedule appointments, and access important documents – all in one place. Streamline your workflow and enhance your productivity with our intuitive tools and features.</p>
+						<h2 class="mt-3">Connect with Clients</h2>
+						<p>Connect with potential clients looking for legal assistance through our platform. Receive and respond to inquiries, schedule consultations, and engage with clients directly to understand their needs and provide personalized legal solutions. Build lasting relationships and grow your client base with ease.</p>
+						<h2 class="mt-3">Enhance Your Visibility</h2>
+						<p>Enhance your visibility and reach within the legal community and beyond. Showcase your expertise, experience, and achievements on your profile to attract potential clients. Stay updated on the latest legal developments, news, and trends to further establish yourself as a trusted legal authority.</p>
+
+
+
+
+
+						</div>
+					</div>
+					<!-- /#page-content-wrapper -->
+					
+				</div>
+				<!-- /#wrapper -->
+				
+				
+				
+			</body>
+			<footer>
+				<div class="container bg-success">
+					<div class="row">
+						<div class="col">
+							<h5>All rights reserved 2022</h5>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<!-- Optional JavaScript -->
+			<!-- jQuery -->
+			
+			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+		</body>
+	</html>		
+	<?php
+		
+	}else 
+	header('location:login.php?deactivate');
+?>	
